4167a1c52f4289c2bb2d4fb7927ffcf13d21e1c8
Dmytro Kalii <ff00d112233@gmail.com>
2021-12-07 00:26:09 +0900

    first_comm

---
+++ src/Car.java
@@ -0,0 +1,34 @@
+public abstract class Car {
+
+    private String model;
+    private String color;
+    private int maxCapacity;
+
+    public abstract void gas();
+
+    public abstract void brake();
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+}
